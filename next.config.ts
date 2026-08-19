9be6885a9a7e94e17ba414ee5f204412e164ca1b
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-08-19 18:07:07 +0300

    Update: next config with image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rickandmortyapi.com",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
